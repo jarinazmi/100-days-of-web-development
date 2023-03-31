e1c4d319c16cbcb4dfd0d0e34314de86f11e0388
jarinazmi <jarinazmi.w3engr@gmail.com>
2023-03-31 23:20:36 +0600

    variable added

---
+++ js-journey/variable.js
@@ -0,0 +1,10 @@
+// number
+var price = 11;
+
+// string
+var name = "jarin";  // we can use double quote
+var district = 'Barishal'; // also we can use single quote
+// var special = "Dipika '; // we can't use this type of format 
+// Boolean
+var isSingle = false;
+var serious = true;
