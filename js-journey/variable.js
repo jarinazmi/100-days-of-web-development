f430b353075bdeeafb4a8219a52e3f44632f2bc1
jarinazmi <jarinazmi.w3engr@gmail.com>
2023-04-01 13:43:25 +0600

    variable name naming convention

--- js-journey/variable.js
+++ js-journey/variable.js
@@ -4,7 +4,29 @@ var price = 11;
 // string
 var name = "jarin";  // we can use double quote
 var district = 'Barishal'; // also we can use single quote
-// var special = "Dipika '; // we can't use this type of format 
+var special = "Dipika '; // we can't use this type of format 
 // Boolean
 var isSingle = false;
 var serious = true;
+
+// 1. variable name can't be any keyword
+var false = 12;
+var return = 96;
+
+// 2. variable name has to be in one word
+
+var my home address = "New california";
+
+// 3. variable name can't have quotation
+
+var "name" = "Tom Hanks";
+
+// 4. Variable name can't starts with a number but can ends with a number
+var 99club = 1964;
+var club25 = 1900;
+
+// 5.How to use long name
+// can't use dash
+var userName = "Raj Bappa"; // camel case :we will use this
+var UserName = "Raj Bappa"; // pascal case
+var user_name = "Raj Bappa"; // snake case
