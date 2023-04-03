ace81dfe418c8b54ae712939bffa77b923b442ce
jarinazmi <jarinazmi.w3engr@gmail.com>
2023-04-03 23:59:52 +0600

    shorhand

---
+++ js-journey/shorthand.js
@@ -0,0 +1,12 @@
+// variable declaration
+var tomatoPrice = 25;
+//variable value update
+tomatoPrice = 30;
+
+// add something to the previous value
+var price1 = 31;
+var price2 = 10;
+
+// add some value to the exsisting value
+price1 = price1 + 10;
+console.log(price1);
