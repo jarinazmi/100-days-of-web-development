ebeda389a5a9cfd58f71d04fc0ebf568b400ee8a
jarinazmi <jarinazmi.w3engr@gmail.com>
2023-04-05 16:12:12 +0600

    intro practice 2 is added here

---
+++ js-journey/introduction-to-javascript/practice-problem-2.js
@@ -0,0 +1,10 @@
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var sum = mathematics + biology + chemistry + physics + bangla;
+var average = sum / 5;
+average = average.toFixed(2);
+average = parseFloat(average);
+console.log(average);
