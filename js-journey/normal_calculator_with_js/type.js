c7749fb358f3985e6904531d25702ddaceea7ac3
jarinazmi <jarinazmi.w3engr@gmail.com>
2023-05-17 06:10:13 +0600

    normal calculator function

---
+++ js-journey/normal_calculator_with_js/type.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return (a + b);
+}
+
+console.log(add(5, 10));
+
+let result = add(3, 7) + 6;
+console.log(result);
+
+function odd(c, d) {
+    return (c - d);
+}
+console.log(odd(15, 5));
+
+function mul(p, q) {
+    return (p * q);
+}
+
+console.log(mul(5, 10));
+function div(x, y) {
+    return (x / y);
+}
+console.log(div(30, 5));
